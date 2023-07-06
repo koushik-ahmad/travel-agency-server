0b3479a908af524c550acb0ec16e19ba4e6022d3
koushik-ahmad <koushikahmad123@gmail.com>
2023-07-06 14:26:22 +0600

    added some data

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ app.use(cors());
 
 const Port = process.env.PORT || 5000;
 
+const place = require('/data/place.json');
 
 app.get('/', (req, res) => {
     res.send('Agency server is running')
 });
+
+app.get('/allPlace', (req, res) => {
+    res.send(place);
+})
 
 app.listen(Port, () => {
     console.log(`Running on port, ${Port}`);
